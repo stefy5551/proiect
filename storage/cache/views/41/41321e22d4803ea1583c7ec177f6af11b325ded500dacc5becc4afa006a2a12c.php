bde46f6747664677899a65380c57ea92dab8b0dd
stefy5551 <43267991+stefy5551@users.noreply.github.com>
2018-12-11 11:00:29 +0200

    task 4 done

---
+++ storage/cache/views/41/41321e22d4803ea1583c7ec177f6af11b325ded500dacc5becc4afa006a2a12c.php
@@ -0,0 +1,49 @@
+<?php
+
+/* authentification.html */
+class __TwigTemplate_f4f9e07419d9fc67b9ba0aa20e147fbb015bd84334b24d6f4e04db80dcf9e96d extends Twig_Template
+{
+    private $source;
+
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "authentification.html";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  23 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "authentification.html", "D:\\Facultate\\XAMPP\\htdocs\\proiect\\app\\Views\\authentification.html");
+    }
+}
